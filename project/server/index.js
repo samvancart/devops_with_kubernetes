7b9576b38f61f9270c600aa4acf87a5714b85eff
samvancart <sam02cart@gmail.com>
2022-06-03 15:48:21 +0300

    Project responds to GET

--- project/server/index.js
+++ project/server/index.js
@@ -3,6 +3,11 @@ const app = new Koa();
 
 const PORT = process.env.PORT || 3000;
 
+app.use(async ctx => {
+    const message = 'Hello from server!'; 
+    ctx.body = message;
+  });
+
 app.listen(PORT, () => {
     console.log(`Server started in port ${PORT}`)
 });
